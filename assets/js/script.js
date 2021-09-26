41f62955b7df3f0e6b697c784babf35b716c9ffe
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2021-09-26 14:41:42 +0000

    Set up structure for chat functions

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+function runChat() {
+
+}
+
+function displayChat() {
+
+}
+
+function userName() {
+
+}
+
+function checkSelectedButton() {
+
+}
+
+function awesome() {
+
+}
+
+function terrible() {
+
+}
+
+function reasons() {
+
+}
+
+function angry() {
+
+}
+
+function sad() {
+
+}
+
+function tired() {
+
+}
+
+function worthless() {
+
+}
+
+function feedback() {
+
+}
+
