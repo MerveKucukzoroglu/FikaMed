5a7edb86067ba668644bb0d375c6260995a4d33f
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2021-09-26 16:06:38 +0000

    Create chat beginning

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,22 @@
-function runChat() {
+//Wait for DOM to finish loading before running the Chat
 
+document.addEventListener("DOMContentLoaded", function() {
+    runChat();
+})
+
+/**The main chat loop, when the script is loaded
+ * and after user's response
+ */
+function runChat() {
+displayChat();
 }
 
+/**The initial of chat
+ * user enter their name
+ */
 function displayChat() {
-
+    
+    
 }
 
 function userName() {
