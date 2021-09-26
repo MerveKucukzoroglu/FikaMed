ff295611a2373a0760e09d2c4d0dee1cd20040fe
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2021-09-26 20:10:03 +0000

    Add code to initiate chat

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,25 @@
-//Wait for DOM to finish loading before running the Chat
-
-document.addEventListener("DOMContentLoaded", function() {
-    runChat();
-})
 
 /**The main chat loop, when the script is loaded
  * and after user's response
  */
 function runChat() {
-displayChat();
-}
+console.log("hey");
 
+let name = document.getElementsByClassName("user-name").value;
+
+
+document.getElementsByClassName("text-area").innerHTML = name;
+console.log(name);
+
+}
 /**The initial of chat
  * user enter their name
  */
 function displayChat() {
-    
-    
+  console.log("heyw");
 }
 
-function userName() {
 
-}
 
 function checkSelectedButton() {
 
