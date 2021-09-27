1219c6ddb67e0dd007ced44940c29f36b56fa848
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2021-09-27 15:00:27 +0000

    Add enter key function and ready for user to type

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,25 @@
+//Wait for the DOM to finish loading before running
+document.getElementById("user-name").value = "";
+document.getElementById("user-name").focus();
+
+document.getElementById("user-name").addEventListener("keydown", function(event) {
+    if(event.key === "Enter") {
+      event.preventDefault();
+      document.getElementById("submit-button").click();
+    }
+});
 
 /**The main chat loop, when the script is loaded
  * and after user's response
  */
 function runChat() {
-console.log("hey");
-
-let name = document.getElementById("user-name").value;
-
-
-document.getElementById("text-area").innerHTML = `<p>Hey ${name} how are you doing today?</p>`;
-console.log(name);
+  
+  console.log("Begin talking with user!");
+  
+  let name = document.getElementById("user-name").value;
+  document.getElementById("text-area").innerHTML = `<p>Hey ${name}, how are you feeling today?</p>`;
+  
+  console.log(name);
 
 }
 /**The initial of chat
